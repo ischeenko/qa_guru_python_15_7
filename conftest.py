b4a3a8a800dbbfd59d26264bcad3cba1dc3f5278
Alexander Ischenko <ischeenko@gmail.com>
2024-08-08 14:07:47 +0300

    Add pypdf and openpyxl to requirements.txt

--- conftest.py
+++ conftest.py
@@ -2,18 +2,18 @@ import zipfile
 import pytest
 import os
 import shutil
-import pathes
+import paths
 
 
 @pytest.fixture(scope='function', autouse=True)
 def given_zip_file():
-    if not os.path.exists(pathes.ZIPPED_PATH):
-        os.mkdir(pathes.ZIPPED_PATH)
-    with zipfile.ZipFile(pathes.ZIPPED_PATH + '/zipped_files.zip', 'w') as zf:
-        for file in os.listdir(pathes.RESOURCES_PATH):
-            add_file = os.path.join(pathes.RESOURCES_PATH, file)
+    if not os.path.exists(paths.ZIPPED_PATH):
+        os.mkdir(paths.ZIPPED_PATH)
+    with zipfile.ZipFile(paths.ZIPPED_PATH + '/zipped_files.zip', 'w') as zf:
+        for file in os.listdir(paths.RESOURCES_PATH):
+            add_file = os.path.join(paths.RESOURCES_PATH, file)
             zf.write(add_file, os.path.basename(add_file))
 
     yield
 
-    shutil.rmtree(pathes.ZIPPED_PATH)
+    shutil.rmtree(paths.ZIPPED_PATH)
